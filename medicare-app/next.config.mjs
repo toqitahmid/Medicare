4d3877ba6a08403e2bac437802b5a2eca472dcc2
toqitahmid <rubaiathali@gmail.com>
2026-08-20 18:28:44 +0600

    resolve imgbb issue

--- medicare-app/next.config.mjs
+++ medicare-app/next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      }
+    ]
+  }
+  
 };
 
 export default nextConfig;
