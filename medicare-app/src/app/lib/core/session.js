1eeda024bbff870107bf4d58facbcf5222964de5
toqitahmid <rubaiathali@gmail.com>
2026-08-21 17:12:10 +0600

    create get session method for user's visibilty

---
+++ medicare-app/src/app/lib/core/session.js
@@ -0,0 +1,19 @@
+'use server'
+import { headers } from "next/headers"
+import { auth } from "../auth";
+import { redirect } from "next/navigation";
+
+export const getUserSession = async() => {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    return session?.user || null;
+}
+
+export const requiredRole = async(role) => {
+    const user = await getUserSession();
+    if(user.role !== role){
+        return redirect('/unauthorized');
+    }
+}
