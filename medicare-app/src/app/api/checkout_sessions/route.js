c9e5ec0b3f658f9f289770343280c7aa7e8bf06e
toqitahmid <rubaiathali@gmail.com>
2026-08-24 17:30:58 +0600

    dynamic stripe plan for eatch paitents

--- medicare-app/src/app/api/checkout_sessions/route.js
+++ medicare-app/src/app/api/checkout_sessions/route.js
@@ -1,19 +1,26 @@
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
-import { stripe } from "@/app/lib/stripe";
+import { PLAN_PRICE_ID, stripe } from "@/app/lib/stripe";
+import { getUserSession } from "@/app/lib/core/session";
 
 
-export async function POST() {
+export async function POST(request) {
   try {
     const headersList = await headers();
     const origin = headersList.get("origin");
 
+    const formData = await request.formData();
+    const planId = formData.get('planId');
+    const priceId = PLAN_PRICE_ID[planId];
+
+    const user = await getUserSession();
+
     // Create Checkout Sessions from body params.
     const session = await stripe.checkout.sessions.create({
+      customer_email: user?.email,
       line_items: [
         {
-          // Provide the exact Price ID (for example, price_1234) of the product you want to sell
-          price: "price_1U7oU4AZBN3J3tTaeeOgfcQ4",
+          price: priceId,
           quantity: 1,
         },
       ],
--- medicare-app/src/app/lib/stripe.js
+++ medicare-app/src/app/lib/stripe.js
@@ -3,3 +3,10 @@ import "server-only";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+
+export const PLAN_PRICE_ID = {
+  starter: "price_1U7oU4AZBN3J3tTaeeOgfcQ4",
+  standard: "price_1U7qSbAZBN3J3tTahRzfshRG",
+  family: "price_1U7qUVAZBN3J3tTafVW0Q3cT",
+};
