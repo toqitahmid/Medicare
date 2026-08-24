01dc29404591b10b495692f59e2c90b76b1fa0c9
toqitahmid <rubaiathali@gmail.com>
2026-08-24 11:21:43 +0600

    create pateint api helper

---
+++ medicare-app/src/app/lib/api/patients.js
@@ -0,0 +1,25 @@
+'use server';
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+
+export async function getPatientByUserId(userId) {
+    try{
+        const res = await fetch(`${baseUrl}/api/patients/${userId}`,{
+            cache: "no-store"
+        });
+
+        if(!res.ok){
+            console.error(`failed to fetch patients ${res.status}`);
+            return null;
+        }
+        return res.json();
+    }
+    catch(err){
+        console.error(err);
+    }
+}
+
+export async function getPatientInfo (id) {
+    const patient = await getPatientByUserId(id);
+    return patient;
+}
