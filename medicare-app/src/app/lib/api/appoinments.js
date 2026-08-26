8e5bd94c41728e444fccbc059d0a7fc013645eba
toqitahmid <rubaiathali@gmail.com>
2026-08-26 12:07:57 +0600

    create get appointments by it's doctor id api helper

--- medicare-app/src/app/lib/api/appoinments.js
+++ medicare-app/src/app/lib/api/appoinments.js
@@ -1,23 +1,33 @@
-'use server';
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL; 
+"use server";
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-
-export async function getAllAppointments () {
-    try{
-        const res = await fetch(`${baseUrl}/api/appoinments`);
-        if(!res.ok){
-            console.log(`Failed to fetch doctors ${res.status}`)
-        }
-        return res.json();
+export async function getAllAppointments() {
+  try {
+    const res = await fetch(`${baseUrl}/api/appoinments`);
+    if (!res.ok) {
+      console.log(`Failed to fetch doctors ${res.status}`);
     }
-    catch(err){
-        console.err("error", err);
-    }
+    return res.json();
+  } catch (err) {
+    console.err("error", err);
+  }
 }
 
 export async function getAppointmentById(patientId) {
   try {
     const res = await fetch(`${baseUrl}/api/appointments/${patientId}`);
+    if (!res.ok) {
+      console.log(`Failed to fetch appointments ${res.status}`);
+    }
+    return res.json();
+  } catch (err) {
+    console.error("error: ", err);
+  }
+}
+
+export async function getAppointmentByDoctorId(doctorId) {
+  try {
+    const res = await fetch(`${baseUrl}/api/doctors/appointments/${doctorId}`);
     if (!res.ok) {
       console.log(`Failed to fetch appointments ${res.status}`);
     }
