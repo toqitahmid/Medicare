f9864f317f54f729f30b7573b494486fe49f4bc3
toqitahmid <rubaiathali@gmail.com>
2026-08-26 21:12:54 +0600

    create not found and unauthorized page for valid users

---
+++ medicare-app/src/app/unauthorized/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="h-screen flex justify-center items-center text-4xl font-bold">
+        {" "}
+        Unauthorized User
+      </h1>
+    </div>
+  );
+};
+
+export default page;
