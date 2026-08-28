9bd700760f2ccda346b92f3aed2cac22571bc6e3
toqitahmid <rubaiathali@gmail.com>
2026-08-28 19:46:17 +0600

    check user avibility

--- medicare-app/src/app/lib/core/session.js
+++ medicare-app/src/app/lib/core/session.js
@@ -1,19 +1,24 @@
-'use server'
-import { headers } from "next/headers"
+"use server";
+import { headers } from "next/headers";
 import { auth } from "../auth";
 import { redirect } from "next/navigation";
 
-export const getUserSession = async() => {
+export const getUserSession = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  return session?.user || null;
+};
 
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
-    return session?.user || null;
-}
+export const requiredRole = async (role) => {
+  const user = await getUserSession();
+  console.log("=== requiredRole DEBUG ===");
+  console.log("Required role passed in:", role);
+  console.log("Actual user object:", user);
+  console.log("user?.role:", user?.role);
+  console.log("Match?", user?.role === role);
 
-export const requiredRole = async(role) => {
-    const user = await getUserSession();
-    if(user?.role !== role){
-        return redirect('/unauthorized');
-    }
-}
+  if (user?.role !== role) {
+    return redirect("/unauthorized");
+  }
+};
