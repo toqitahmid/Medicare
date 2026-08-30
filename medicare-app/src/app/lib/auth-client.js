9856e492f6f9a8c7159529a0177cc96025d27463
toqitahmid <rubaiathali@gmail.com>
2026-08-30 21:23:47 +0600

    set BETTER_AUTH_URL in auth-client.js

--- medicare-app/src/app/lib/auth-client.js
+++ medicare-app/src/app/lib/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [
     inferAdditionalFields({
       user: {
